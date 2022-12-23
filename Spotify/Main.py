2163f302aa1ce8e29d2a961444f085c980651603
ZentyNhan <wayne.j.n19@gmail.com>
2022-12-24 01:49:07 +0700

    Dec 24th 2022 : update

--- Spotify/Main.py
+++ Spotify/Main.py
@@ -1,6 +1,7 @@
 ########## SECTION: Library ##########
 from ast import Try, keyword
 from contextlib import nullcontext
+from lib2to3.pgen2.literals import test
 import os
 from pickle import FALSE, TRUE
 from subprocess import check_output
@@ -24,7 +25,6 @@ from termcolor import colored
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 #Excel Processing:
 from openpyxl import Workbook 
 from openpyxl.worksheet.worksheet import Worksheet
@@ -32,36 +32,65 @@ from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Alignment, Font,PatternFill,Border,Side
 
-#MySQL:
-import mysql.connector
-from mysql.connector import Error
+#pypyodbc:
+import pypyodbc as odbc
+import pyodbc
+
+DRIVER_NAME = 'ODBC Driver 17 for SQL Server'
+SERVER_NAME = 'DESKTOP-RCFES7K\SQLEXPRESS'
+DATABASE_NAME = 'SpotifyUpgrade_DB'
+
+connection = f'''
+        DRIVER={DRIVER_NAME};
+        SERVER={SERVER_NAME};
+        DATABASE={DATABASE_NAME};
+        UID=Thanhnhan19;
+        PWD=Nhan0334842024;
+'''
+#connect to database:
+conn = pyodbc.connect(connection)
+
+#use cursor:
+cursor = conn.cursor()
+
+#Extract data from
+#Local var
+Email = []
+PassW = []
+
+for row in cursor.execute('select * from Customer_Infor'):
+        Email.append(row.EmailAddress)
+        PassW.append(row.EmailPW)
+
+print('Email: ',Email[0])
+print('PassW: ',PassW[0])
+
+driver = webdriver.Chrome('chromedriver.exe')
+url = 'https://whoer.net/fr'
+#Get link:
+driver.get(url)
+
+url = f'https://accounts.spotify.com/vi-VN/login?continue=https%3A%2F%2Fopen.spotify.com%2F'
+#Get link:
+driver.get(url)
+
+driver.maximize_window()
+sleep(3)
+driver.find_element(By.ID ,'login-username').send_keys(Email[0])
+driver.find_element(By.ID ,'login-password').send_keys(PassW[0])
+driver.find_element(By.ID ,'login-button').click()
+sleep(3)
+driver.get('https://www.spotify.com/tr/account/profile/')
+sleep(3)
+select = Select(driver.find_element(By.ID, 'country'))
+select.select_by_value('TR')
+
+driver.find_element(By.XPATH, '''//body/div[@id='__next']/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/article[1]/section[1]/form[1]/div[1]/button[1]''').click()
 
 
-try:
-    connection = mysql.connector.connect(host='localhost',
-                                         database='Electronics',
-                                         user='pynative',
-                                         password='pynative@#29')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
-
-
-
-
-
+# //body/div[@id='__next']/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/article[1]/section[1]/form[1]/div[1]/button[1]
+# //span[contains(text(),'Profili kaydet')]
+input('Press any key to break')
 
 
 
